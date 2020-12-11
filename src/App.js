ee569400c9882e52701c017136ddf6b450a81d5f
yash1703 <50240587+yash1703@users.noreply.github.com>
2020-12-12 01:24:30 +0530

    style : style updated

--- src/App.js
+++ src/App.js
@@ -3,32 +3,53 @@ import "./styles.css";
 
 const gameDB = {
   "Action 🔥": [
-    { name: "Grand Theft Auto V", rating: "4/5" },
-    { name: "Watch Dogs", rating: "3.5/5" },
-    { name: "HITMAN 2", rating: "4/5" }
+    {
+      name: "Grand Theft Auto V",
+      rating: "4.8/5",
+      summary:
+        "Set within the fictional state of San Andreas, based on Southern California, the single-player story follows three protagonists—retired bank robber Michael De Santa, street gangster Franklin Clinton, and drug dealer and arms smuggler Trevor Philips—and their efforts to commit heists while under pressure from a corrupt ...",
+      link: "https://en.wikipedia.org/wiki/Grand_Theft_Auto_V"
+    },
+    {
+      name: "Watch Dogs",
+      rating: "4.2/5",
+      summary:
+        "Watch Dogs is a series of action-adventure games developed and published by Ubisoft. It spans three games: Watch Dogs, Watch Dogs 2 and Watch Dogs: Legion. Gameplay focuses on an open world where the player can complete missions to progress an overall story, as well as engage in various side activities.",
+      link: "https://en.wikipedia.org/wiki/Watch_Dogs"
+    }
   ],
-
   "FPS 🔫": [
     {
       name: "Counter Strike",
-      rating: "5/5"
+      rating: "5/5",
+      summary:
+        "Counter-Strike is a series of multiplayer first-person shooter video games in which teams of terrorists battle to perpetrate an act of terror while counter-terrorists try to prevent it. The series began on Windows in 1999 with the release of the first game, Counter-Strike.",
+      link: "https://en.wikipedia.org/wiki/Counter-Strike"
     },
     {
       name: "PlayerUnknown's Battlegrounds ",
-      rating: "4.5/5"
-    },
-    { name: "APEX Legends", rating: "3.5/5" }
+      rating: "4.5/5",
+      summary:
+        "Battlegrounds is a player versus player shooter game in which up to one hundred players fight in a battle royale, a type of last man standing deathmatch where players fight to remain the last alive. Players can choose to enter the match solo, duo, or with a small team of up to four people. The last person or team alive wins the match.",
+      link: "https://en.wikipedia.org/wiki/PlayerUnknown's_Battlegrounds"
+    }
   ],
+
   "Sports ⚽": [
     {
-      name: "FIFA 21",
-      rating: "3.5/5"
+      name: "FIFA 22",
+      rating: "5/5",
+      summary:
+        "The 2022 FIFA World Cup is scheduled to be the 22nd edition of the FIFA World Cup, the quadrennial international men's association football championship contested by the national teams of the member associations of FIFA. It is scheduled to take place in Qatar from 21 November to 18 December 2022.",
+      link: "https://en.wikipedia.org/wiki/2022_FIFA_World_Cup"
     },
     {
       name: "Pro Evolution Soccer",
-      rating: "5/5"
-    },
-    { name: "NBA 2K20", rating: "4/5" }
+      rating: "4.1/5",
+      summary:
+        "eFootball PES 2020 is a football simulation video game developed by PES Productions and published by Konami for Microsoft Windows, PlayStation 4, Xbox One, Android and IOS.",
+      link: "https://en.wikipedia.org/wiki/Pro_Evolution_Soccer"
+    }
   ]
 };
 
@@ -84,7 +105,13 @@ export default function App() {
                   }}
                 >
                   <div style={{ fontSize: "1.3rem" }}>{gamedis.name}</div>
-                  <div style={{ fontSize: ".8rem" }}>{gamedis.rating}</div>
+                  <div style={{ fontSize: ".8rem", padding: ".4rem 0rem" }}>
+                    Rating: {gamedis.rating}
+                  </div>
+                  <div style={{ fontSize: "1rem" }}>
+                    {gamedis.summary}
+                    <a href={gamedis.link}> Read more</a>
+                  </div>
                 </li>
               );
             })}
